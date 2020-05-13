f988c638de54583da295ef3ac88695836273f5af
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-13 09:20:47 -0600

    Initial commit.  Fixed initial tests.  Added test_it_can_be_painted test and associated method.

--- lib/room.rb
+++ lib/room.rb
@@ -1,12 +1,25 @@
-class Rom
+class Room
+  attr_reader :category,
+              :is_painted
 
   def initialize(category, length, width)
     @category = category
-    @length = 10
-    @width = width
+    @length = length.to_i
+    @width = width.to_i
+    @is_painted
+
   end
 
   def area
     @length * @width
   end
+
+  def is_painted?
+    @is_painted
+  end
+
+  def paint
+    @is_painted = true
+  end
+
 end
