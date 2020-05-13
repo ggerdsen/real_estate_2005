9d815de92ad568ddc86557913e8dbce2f11e0b40
Garrett Gerdsen <garrett.gerdsen@gmail.com>
2020-05-13 09:33:30 -0600

    Set up house test and class

---
+++ lib/house.rb
@@ -0,0 +1,19 @@
+class House
+  attr_reader :price,
+              :address
+              :rooms
+
+
+  def initialize(price, address)
+    @price = price
+    @address = address
+    @rooms = []
+  end
+
+  def rooms
+    @rooms
+  end
+
+
+
+end
---
+++ test/house_test.rb
@@ -0,0 +1,31 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/room'
+require './lib/house'
+require 'pry'
+
+
+class HouseTest < Minitest::Test
+
+  def test_it_exists
+    house = House.new("$400000", "123 sugar lane")
+    assert house
+  end
+
+  def test_it_has_a_price
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal "$400000", house.price
+  end
+
+  def test_it_has_an_address
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal "123 sugar lane", house.address
+  end
+
+  def test_it_has_rooms
+    house = House.new("$400000", "123 sugar lane")
+    assert_equal [], house.rooms
+
+  end
+
+end
